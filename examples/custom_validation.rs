26fc7e95785ddeb20debf730819a66698ef9dcb1
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2023-03-31 22:47:06 +0200

    new: Obtain context from request

---
+++ examples/custom_validation.rs
@@ -0,0 +1,55 @@
+//! Simple usage of using `axum_garde` for a REST API
+//!
+//! Run the example using
+//!
+//! ```sh
+//! cargo run --example json
+//! ```
+use axum::{response::IntoResponse, routing::post, Json, Router, Server};
+use axum_garde::WithValidation;
+use garde::Validate;
+use serde::{Deserialize, Serialize};
+
+// Define your valid scheme
+#[derive(Debug, Serialize, Deserialize, Validate)]
+#[garde(context(PasswordContext))]
+struct Person {
+    #[garde(ascii, length(min = 3, max = 25))]
+    username: String,
+    #[garde(custom(password_validation))]
+    password: String,
+}
+
+// Define your custom context
+#[derive(Debug, Clone)]
+struct PasswordContext {
+    complexity: usize,
+}
+
+// Define your custom validation
+fn password_validation(value: &str, context: &PasswordContext) -> garde::Result {
+    if value.len() < context.complexity {
+        return Err(garde::Error::new("password is not strong enough"));
+    }
+    Ok(())
+}
+
+async fn custom_validation(
+    // Perform validation on the request payload
+    WithValidation(_, _): WithValidation<Person, Json<Person>>,
+) -> impl IntoResponse {
+    "Validation suceeed!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/check_password", post(custom_validation))
+        // Create the application state
+        .with_state(PasswordContext { complexity: 10 });
+    println!("See example: http://127.0.0.1:8080/check_password");
+    Server::bind(&([127, 0, 0, 1], 8080).into())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
--- src/with_validation.rs
+++ src/with_validation.rs
@@ -4,6 +4,7 @@ use super::Unwrapable;
 use super::WithValidationRejection;
 
 use axum::async_trait;
+use axum::extract::FromRef;
 use axum::extract::FromRequest;
 use axum::extract::FromRequestParts;
 use axum::http::request::Parts;
@@ -22,11 +23,12 @@ impl<T, E> From<Valid<T>> for WithValidation<T, E> {
 }
 
 #[async_trait]
-impl<S, T, E> FromRequestParts<S> for WithValidation<T, E>
+impl<S, T, E, CONTEXT> FromRequestParts<S> for WithValidation<T, E>
 where
     S: Send + Sync,
     E: FromRequestParts<S> + Unwrapable<T>,
-    T: Validate<Context = ()>,
+    T: Validate<Context = CONTEXT>,
+    CONTEXT: FromRef<S>,
 {
     type Rejection = WithValidationRejection<E::Rejection>;
 
@@ -35,20 +37,22 @@ where
             .await
             .map_err(WithValidationRejection::ExtractionError)?;
         let t: T = extracted.extract();
+        let state = FromRef::from_ref(state);
         let valid = Unvalidated::from(t)
-            .validate(&())
+            .validate(&state)
             .map_err(WithValidationRejection::ValidationError)?;
         Ok(WithValidation(valid, Default::default()))
     }
 }
 
 #[async_trait]
-impl<S, B, T, E> FromRequest<S, B> for WithValidation<T, E>
+impl<S, B, T, E, CONTEXT> FromRequest<S, B> for WithValidation<T, E>
 where
     B: Send + 'static,
     S: Send + Sync,
     E: FromRequest<S, B> + Unwrapable<T>,
-    T: Validate<Context = ()>,
+    T: Validate<Context = CONTEXT>,
+    CONTEXT: FromRef<S>,
 {
     type Rejection = WithValidationRejection<E::Rejection>;
 
@@ -58,8 +62,9 @@ where
             .map_err(WithValidationRejection::ExtractionError)?;
 
         let t: T = extracted.extract();
+        let state = FromRef::from_ref(state);
         let valid = Unvalidated::from(t)
-            .validate(&())
+            .validate(&state)
             .map_err(WithValidationRejection::ValidationError)?;
         Ok(WithValidation(valid, Default::default()))
     }
